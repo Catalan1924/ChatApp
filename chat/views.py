39e967b638cf9035bac464e6586f01f9782853e3
Shem <oduor2shem@gmail.com>
2025-08-23 18:32:29 +0300

    views

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import ChatRoom, Message
 
-# Create your views here.
+def home(request):
+    rooms = ChatRoom.objects.all()
+    return render(request, 'chat/home.html', {'rooms': rooms})
+
+def room(request, room_name):
+    room, created = ChatRoom.objects.get_or_create(name=room_name)
+    messages = Message.objects.filter(room=room)
+    return render(request, 'chat/room.html', {'room': room, 'messages': messages})
