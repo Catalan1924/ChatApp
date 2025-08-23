88170ab9a9a569e2518799192e8d70c002402c25
Shem <oduor2shem@gmail.com>
2025-08-23 18:50:32 +0300

    chat room

--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import ChatRoom, Message
 
+@login_required
 def home(request):
     rooms = ChatRoom.objects.all()
+
+    # Kama user akiingiza room name kwa homepage
+    if request.method == "GET" and 'room_name' in request.GET:
+        room_name = request.GET.get('room_name')
+        return redirect('room', room_name=room_name)
+
     return render(request, 'chat/home.html', {'rooms': rooms})
 
+@login_required
 def room(request, room_name):
     room, created = ChatRoom.objects.get_or_create(name=room_name)
-    messages = Message.objects.filter(room=room)
+
+    if request.method == "POST":
+        message = request.POST.get('message')
+        if message.strip():
+            Message.objects.create(room=room, user=request.user, content=message)
+        return redirect('room', room_name=room_name)
+
+    messages = Message.objects.filter(room=room).order_by('timestamp')
     return render(request, 'chat/room.html', {'room': room, 'messages': messages})
