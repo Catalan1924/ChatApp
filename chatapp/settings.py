b48789eb7d0c1a03c4964d736d9857ebfcd974cf
Shem <oduor2shem@gmail.com>
2025-08-26 19:10:43 +0300

    hosts

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(",")
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','').split(",")
 
 
 # Application definition
