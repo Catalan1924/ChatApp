7bc7007a17934a48e146fe170c6349c07f9f9cdd
Shem <oduor2shem@gmail.com>
2025-08-26 21:43:28 +0300

    dj_database_url

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'chatapp.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
+    'default': dj_database_url.config (
         default=os.getenv("DATABASE_URL"),
         conn_max_age=600
     )
